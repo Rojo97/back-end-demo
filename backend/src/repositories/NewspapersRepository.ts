ccddc81479375080c6059483fee8fac21fed1bc2
Víctor Rojo <victor.rojoalvarez@telefonica.com>
2022-09-17 20:07:34 +0200

    feat: add pagination

--- backend/src/repositories/NewspapersRepository.ts
+++ backend/src/repositories/NewspapersRepository.ts
@@ -1,9 +1,10 @@
+import { PaginateResult } from "mongoose";
 import { NewspaperInterface } from "../models/Newspaper";
 
 export interface NewspapersRepository {
-  getAllNewspapers(): Promise<Array<NewspaperInterface>>;
-  getNewspaperById(id: string): Promise<NewspaperInterface>;
+  getAllNewspapers(limit: number, skip: number): Promise<PaginateResult<NewspaperInterface>>;
+  getNewspaperById(id: number): Promise<NewspaperInterface>;
   createNewspaper(newspaper: NewspaperInterface): Promise<NewspaperInterface>;
-  updateNewspaper(id: string, newspaper: NewspaperInterface): Promise<NewspaperInterface>;
-  deleteNewspaper(id: string): Promise<NewspaperInterface>;
+  updateNewspaper(id: number, newspaper: NewspaperInterface): Promise<NewspaperInterface>;
+  deleteNewspaper(id: number): Promise<NewspaperInterface>;
 }
--- backend/src/repositories/impl/NewspapersRepositoryImpl.ts
+++ backend/src/repositories/impl/NewspapersRepositoryImpl.ts
@@ -1,31 +1,40 @@
+import { PaginateResult } from "mongoose";
 import { Newspaper, NewspaperInterface } from "../../models/Newspaper";
 import { NewspapersRepository } from "../NewspapersRepository";
 
+const PUBLISHER = "publisher";
+
 export class NewspapersRepositoryImpl implements NewspapersRepository {
-  async getAllNewspapers(): Promise<Array<NewspaperInterface>> {
-    return Newspaper.find().populate("publisher");
+  async getAllNewspapers(
+    limit: number,
+    skip: number
+  ): Promise<PaginateResult<NewspaperInterface>> {
+    return Newspaper.paginate(
+      {},
+      { populate: PUBLISHER, limit: limit, offset: skip }
+    );
   }
 
-  async getNewspaperById(id: string): Promise<NewspaperInterface> {
-    return Newspaper.findById(id).populate("publisher");
+  async getNewspaperById(id: number): Promise<NewspaperInterface> {
+    return Newspaper.findById(id).populate(PUBLISHER);
   }
 
   async createNewspaper(
     newspaper: NewspaperInterface
   ): Promise<NewspaperInterface> {
-    return (await new Newspaper(newspaper).save()).populate("publisher");
+    return (await new Newspaper(newspaper).save()).populate(PUBLISHER);
   }
 
   async updateNewspaper(
-    id: string,
+    id: number,
     newspaper: NewspaperInterface
   ): Promise<NewspaperInterface> {
     return Newspaper.findByIdAndUpdate(id, newspaper)
-      .setOptions({"returnDocument": "after"})
-      .populate("publisher");
+      .setOptions({ returnDocument: "after" })
+      .populate(PUBLISHER);
   }
 
-  async deleteNewspaper(id: string): Promise<NewspaperInterface> {
-    return Newspaper.findByIdAndDelete(id).populate("publisher");
+  async deleteNewspaper(id: number): Promise<NewspaperInterface> {
+    return Newspaper.findByIdAndDelete(id).populate(PUBLISHER);
   }
 }
